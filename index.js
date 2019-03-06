b40955d3374a1cba123dcac9e918e9edef050493
Lorenzo Llamas <ltl924@gmail.com>
2019-03-06 22:13:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+artists = ['John Lennon','Paul McCartney', 'George Harrison', 'Ringo Starr'];
+instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums']
+
+function theBeatlesPlay(artist_arr,instrument_arr) {
+  let sentences = [];
+  for (let i = 0; i < artist_arr.length; i++) {
+    sentences.push(artist_arr[i] + ' plays ' + instrument_arr[i]);
+  }
+  console.log(sentences);
+  return sentences
+}
+theBeatlesPlay(artists,instruments)
